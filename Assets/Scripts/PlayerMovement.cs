c5cd88e7dd39b7acba338ca484903b84dd30b6f7
Vallvoron <165002510+Vallvoron@users.noreply.github.com>
2024-06-01 20:03:43 +0700

    I need more bullets

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int currentHealth;
     public float moveSpeed;
     public Rigidbody2D rb;
     private Vector2 moveDirection;
-    public HealthBar healthBar;
 
-    void Start()
-    {
-        currentHealth = maxHealth;
-        healthBar.ChangeMaxHealth(maxHealth);
-    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TakeDamage(20);
-        }
         ProcessInputs();
     }
 
@@ -42,10 +30,5 @@ public class PlayerMovement : MonoBehaviour
     void Move()
     {
         rb.velocity = new Vector2(moveDirection.x* moveSpeed, moveDirection.y*moveSpeed);
-    }
-    void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
-        healthBar.ChangeHealth(currentHealth);
     }
 }
--- Assets/Scripts/PlayerRotate.cs
+++ Assets/Scripts/PlayerRotate.cs
@@ -6,6 +6,11 @@ public class PlayerRotate : MonoBehaviour
 {
     Rigidbody2D rb;
     Camera SceneCamera;
+    public GameObject bullet;
+    public Transform startPoint;
+    public float offset;
+    private float timeCheck;
+    public float atkSpeed;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,9 +20,19 @@ public class PlayerRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 mousePos = SceneCamera.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 aimPos = mousePos - rb.position;
-        float playerRotation = Mathf.Atan2(aimPos.y, aimPos.x) * Mathf.Rad2Deg;
-        rb.rotation = playerRotation;
+        Vector3 mousePos = SceneCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 diff = mousePos - transform.position;
+        float playerRotation = Mathf.Atan2(diff.y,  diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, playerRotation- offset);
+
+        if (timeCheck <= 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(bullet, startPoint.position, transform.rotation);
+                timeCheck = atkSpeed;
+            }
+        }
+        else timeCheck -= Time.deltaTime;
     }
 }
