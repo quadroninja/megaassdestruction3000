6ebcb71eb83163f4159f2dcbcc1ef9433bab2755
quadroninja <feckfeckfeck@mail.ru>
2024-06-29 10:32:24 +0700

    hookerChange

--- Assets/Scripts/EnemyBehavior/Hook.cs
+++ Assets/Scripts/EnemyBehavior/Hook.cs
@@ -7,25 +7,19 @@ public class Hook : MonoBehaviour
     public float speed;
     public float lifetime;
     public float distance;
-    public bool hookable;
     public float pullForce;
     public float tickTime;
     private float timeCheck;
     private GameObject player;
     public LayerMask whatIsSolid;
 
-    void Start()
-    {
-        hookable=false;
-    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player") && timeCheck < 0)
         {
-            Debug.Log("Hooked");
             player = other.gameObject;
-            hookable = true;
-            gameObject.transform.up = new Vector2(Vector2.up.x-180f, Vector2.up.y-180f);
+            player.GetComponent<PlayerMovement>().getHooked(this.gameObject);
+            //gameObject.transform.up = new Vector2(Vector2.up.x-180f, Vector2.up.y-180f);
             timeCheck = tickTime;
         }
     }
@@ -33,18 +27,21 @@ public class Hook : MonoBehaviour
     void Update()
     {
         timeCheck -= Time.deltaTime;
-        if (lifetime<0)
+        if (lifetime < 0)
         {
             Destroy(gameObject);
         }
         else lifetime -= Time.deltaTime;
-        transform.Translate(Vector2.up*speed*Time.deltaTime);
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
     }
     void OnDestroy()
     {
-        if (hookable)
+        if (player.transform.IsChildOf(this.transform))
         {
-            player.transform.position = gameObject.transform.position;
+            player.GetComponent<PlayerMovement>().getUnhooked();
+            foreach (Behaviour comp in player.GetComponents<Behaviour>())
+                comp.enabled = true;
         }
+        //player.transform.position = gameObject.transform.position;    
     }
 }
--- Assets/Scripts/TileBehaviour/PlayerMovement.cs
+++ Assets/Scripts/TileBehaviour/PlayerMovement.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
+
     private float baseMoveSpeed;
     public float moveSpeed;
     public Rigidbody2D rb;
     private float timeCheck;
     private float waitTime;
     private Vector2 moveDirection;
+    private bool isHooked;
+    public bool IsHooked { get { return isHooked; } }
+    public float hookStrengthMultiplier = 20f;
+    public float hookStrengthThreshold = 100f;
+    public float hookStrengthDecreaseOnTap = 10f;
+    private float hookStrengthCurrent = 0f;
+
     public Animator animator;
     // Update is called once per frame
     void Update()
@@ -23,23 +31,41 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
+    public void getHooked(GameObject hook)
+    {
+        this.transform.parent = hook.transform;
+        isHooked = true;
+        hookStrengthCurrent = hookStrengthThreshold;
+    }
+    public void getUnhooked()
+    {
+        transform.parent = null;
+        isHooked = false;
+        hookStrengthCurrent = 0f;
+    }
     void ProcessInputs()
     {
-        moveDirection.x = Input.GetAxisRaw("Horizontal");
-        moveDirection.y = Input.GetAxisRaw("Vertical");
-
-        if (moveDirection.magnitude > 0f)
+        if (isHooked)
         {
-            moveDirection.Normalize();
+            moveDirection = new Vector2(0f, 0f);
+            if (Input.GetKeyDown(KeyCode.Space))
+                hookStrengthCurrent -= hookStrengthDecreaseOnTap;
+            if (hookStrengthCurrent <= 0f)
+                getUnhooked();
+            hookStrengthCurrent = Mathf.Min(hookStrengthThreshold, hookStrengthCurrent + Time.deltaTime * hookStrengthMultiplier);
+            return;
         }
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
 
         animator.SetFloat("Horizontal", moveDirection.x);
         animator.SetFloat("Vertical", moveDirection.y);
         animator.SetFloat("Speed", moveDirection.sqrMagnitude);
     }
-
     void Move()
     {
-        rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.fixedDeltaTime);
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }
